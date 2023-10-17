644ea531ac76cc5154db22197b06f88ee6304fa5
springboot <wlgu1717@naver.com>
2023-10-17 17:03:11 +0900

    security

--- src/main/java/com/basic/myspringboot/config/WebConfig.java
+++ src/main/java/com/basic/myspringboot/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.basic.myspringboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
                 //반드시 m 다음에 / 을 주어야 한다.
                 .addResourceLocations("classpath:/mobile/")
                 .setCachePeriod(20);//20초
+    }
+
+    //Cors
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("*");
     }
 }
